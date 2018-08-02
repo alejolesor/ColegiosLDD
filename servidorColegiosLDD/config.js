61d37690891df9493ea69d190f9c87f361800d13
Jorge H <jorge.home@.cun.edu.co>
2018-08-01 21:56:07 -0500

    Creacion logueo

--- servidorColegiosLDD/config.js
+++ servidorColegiosLDD/config.js
@@ -1,3 +1,4 @@
 module.exports={
+    'secretKey': '123456789-987654321',
     'mongoUrl' : 'mongodb://localhost:27017/claseServidor',
 }
--- servidorColegiosLDD/models/usuario.js
+++ servidorColegiosLDD/models/usuario.js
@@ -1,18 +1,21 @@
 var mongoose=require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 var Esquema=mongoose.Schema;
 var usuario=new Esquema({
     username:{
-        type:String,
-        required:true
+        type:String
     },
     password:String,
     primernombre:String,
     segundonombre:String,
     correo:{
         type:String,
-        required:true,
         validate: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    admin:{
+        type: Boolean,
+        default: false,
     }
 });
-
-module.exports=mongoose.model('Usuario',usuario);
+usuario.plugin(passportLocalMongoose);
+module.exports=mongoose.model('usuario',usuario);
