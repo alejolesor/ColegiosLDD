1dc12768c4ff861e3e9091bd00de961d80941547
Jorge H <jorge.home@.cun.edu.co>
2018-08-08 19:47:54 -0500

    Ingreso por facebook

--- servidorColegiosLDD/config.js
+++ servidorColegiosLDD/config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey': '123456789-987654321',
     'mongoUrl' : 'mongodb://localhost:27017/claseServidor',
+    'facebook' : {
+        clientId: '442501879581632',
+        clientSecret: '9cd90f3fa984e954b4f85ce8b46198a7'
+    }
 }
