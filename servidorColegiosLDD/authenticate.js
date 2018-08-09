6a173b77db458662e4b3f4fd062a9d267f76199f
Jorge H <jorge.home@.cun.edu.co>
2018-08-08 21:29:08 -0500

    Logueo con facebook2

--- servidorColegiosLDD/authenticate.js
+++ servidorColegiosLDD/authenticate.js
@@ -41,6 +41,7 @@ exports.facebookPassport = passport.use(new FacebookTokenStrategy({
     clientID: config.facebook.clientId,
     clientSecret: config.facebook.clientSecret
 }, (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
     User.findOne({facebookId: profile.id}, (err, user) => {
         if (err) {
             return done(err, false);
