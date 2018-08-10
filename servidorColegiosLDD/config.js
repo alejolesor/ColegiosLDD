57b2253828cf7b4ab15a494894756199744473c1
Diegof <dafp18otmail.com>
2018-08-09 20:56:01 -0500

    hello

--- servidorColegiosLDD/config.js
+++ servidorColegiosLDD/config.js
@@ -1,6 +1,6 @@
 module.exports={
     'secretKey': '123456789-987654321',
-    'mongoUrl' : 'mongodb://localhost:27017/claseServidor',
+    'mongoUrl' : process.env.MONGO_DEV || process.env.MONGO_MASTER|| 'mongodb://localhost:27017/claseServidor',
     'facebook' : {
         clientId: '442501879581632',
         clientSecret: '9cd90f3fa984e954b4f85ce8b46198a7'
