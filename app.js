099ec9f23c0df230e8daab518be3acc4defc43b4
brandon <brandonjks@hotmail.com>
2018-08-29 19:43:38 -0500

    agregamos cors y redireccion

--- app.js
+++ app.js
@@ -7,19 +7,26 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var estudianteRouter=require('./routes/estudianteRouter');
+var estudianteRouter = require('./routes/estudianteRouter');
 var directivoRouter = require('./routes/directivoRouter');
-var mongoose=require('mongoose');
-var config=require('./config');
-var db=mongoose.connect(config.mongoUrl);
+var mongoose = require('mongoose');
+var config = require('./config');
+var db = mongoose.connect(config.mongoUrl);
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
-mongoose.connection.on('error',()=>{console.log("Base de datos en problemas")})
-mongoose.connection.once('open',()=>{console.log("Se ha conectado correctamente")})
+var cors = require('cors');
+mongoose.connection.on('error', () => { console.log("Base de datos en problemas") })
+mongoose.connection.once('open', () => { console.log("Se ha conectado correctamente") })
 
 var app = express();
-
+app.use(cors());
+app.all('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] != "https") {
+        res.redirect('307', 'https://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,25 +43,25 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/asdfs',estudianteRouter);
-app.use('/directivo',directivoRouter);
+app.use('/asdfs', estudianteRouter);
+app.use('/directivo', directivoRouter);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
-  res.json(err);
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error');
+    res.json(err);
 });
 
 module.exports = app;
